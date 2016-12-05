a5e7dd47659119988ee8fb17ca9527ee2adede17
Scott Raymond <codemonkey552@gmail.com>
2016-12-04 21:54:27 -0800

    Update about.js

--- about.js
+++ about.js
@@ -1,7 +1,5 @@
 function about() {
-    var item = document.getElementById("about");
     
-    item.onclick = about() {
     var display = 'Personal Portfolio Website'+'\nAuthor: Scott M. Raymond'+'\nThanks for visiting!'
     alert(display);
 }
