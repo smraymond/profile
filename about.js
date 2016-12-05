21194d6edf75673080952e2a527ac7b4f5e1610d
Scott Raymond <codemonkey552@gmail.com>
2016-12-04 21:42:27 -0800

    Update about.js

--- about.js
+++ about.js
@@ -1,11 +1,11 @@
-(function() {
-    document.getElementById("about").onclick = function() {
-    var message = 'Cassini Page and News Viewer'
-                + '\nBy Robert M Mettetal'
+function about() {
+    var item = document.getElementById("about");
+    
+    item.onclick = about() {
+    var display = 'Personal Portfolio Website'+'\nAuthor: Scott M. Raymond'+''
                 + '\n\nFor CSE322 at CSUSB - Fall 2015 - Dr. David Turner'
                 + '\n\nAll relevant rights reserved'
                 + '\nrssFeedScript.js \u00A9 2015 Robert M Mettetal'
                 + '\n\nThis project has no affiliation with NASA/JPL'
-    alert(message);
+    alert(display);
 }
-})();
