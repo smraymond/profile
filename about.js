fd607398f65edb910d3bd3c63b887a2788e3b930
Scott Raymond <codemonkey552@gmail.com>
2016-12-04 21:27:31 -0800

    Create about.js

---
+++ about.js
@@ -0,0 +1,11 @@
+(function() {
+    document.getElementById("about").onclick = function() {
+    var message = 'Cassini Page and News Viewer'
+                + '\nBy Robert M Mettetal'
+                + '\n\nFor CSE322 at CSUSB - Fall 2015 - Dr. David Turner'
+                + '\n\nAll relevant rights reserved'
+                + '\nrssFeedScript.js \u00A9 2015 Robert M Mettetal'
+                + '\n\nThis project has no affiliation with NASA/JPL'
+    alert(message);
+}
+})();
