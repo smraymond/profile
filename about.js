62fbf6fe5dda90c06bfc87d2254e758d02ebe16c
Scott Raymond <codemonkey552@gmail.com>
2016-12-04 21:43:07 -0800

    Update about.js

--- about.js
+++ about.js
@@ -2,10 +2,6 @@ function about() {
     var item = document.getElementById("about");
     
     item.onclick = about() {
-    var display = 'Personal Portfolio Website'+'\nAuthor: Scott M. Raymond'+''
-                + '\n\nFor CSE322 at CSUSB - Fall 2015 - Dr. David Turner'
-                + '\n\nAll relevant rights reserved'
-                + '\nrssFeedScript.js \u00A9 2015 Robert M Mettetal'
-                + '\n\nThis project has no affiliation with NASA/JPL'
+    var display = 'Personal Portfolio Website'+'\nAuthor: Scott M. Raymond'+'\nThanks for visiting!'
     alert(display);
 }
