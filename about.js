8a3f3fa5fef4334f5b662289c4d96dc634200951
Scott Raymond <codemonkey552@gmail.com>
2016-12-04 22:04:15 -0800

    Update about.js

--- about.js
+++ about.js
@@ -1,3 +1,5 @@
+//This function is used to alert a message to the screen by clicking the About button on the nav-bar
+
 function about() {
     
     var display = 'Personal Portfolio Website'+'\nAuthor: Scott M. Raymond'+'\nThanks for visiting!'
