edf0777d0ef86d51dfdea9592a0b48d3815b3929
Scott Raymond <codemonkey552@gmail.com>
2016-12-04 22:22:17 -0800

    Update about.js

--- about.js
+++ about.js
@@ -2,6 +2,14 @@
 
 function about() {
     
-    var display = 'Personal Portfolio Website'+'\nAuthor: Scott M. Raymond'+'\nThanks for visiting!'
+    var display = 'Personal Portfolio Website'
+                   +'\nAuthor: Scott M. Raymond'
+                   +'\n'
+                   +'\nSources for Project Images:'
+                   +'\nNodejs: https://www.echosteg.com/nodejs-util-inherits-how-inheritance-works'
+                   +'\nFreeNAS: http://www.copyerror.com/wp-content/uploads/2014/08/'
+                   +'\nFOG: https://forums.fogproject.org/recent?cid=7&page=8'
+                   +'\n'
+                   +'\nThanks for visiting!'
     alert(display);
 }
