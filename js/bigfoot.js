8be410c20eb0ca42b703a795fd073ab5f84960ac
smraymond <codemonkey552@gmail.com>
2018-04-02 21:33:48 -0700

    Sound changes

--- js/bigfoot.js
+++ js/bigfoot.js
@@ -414,10 +414,13 @@
    * Start the game - reset all variables and entities, spawn platforms and water.
    */
   function startGame() {
-    assetLoader.sounds.gameOver.pause();
     assetLoader.sounds.bg.currentTime = 0;
     assetLoader.sounds.bg.loop = true;
     assetLoader.sounds.bg.play();
+	assetLoader.sounds.gameOver.play();
+    assetLoader.sounds.gameOver2.play();
+    assetLoader.sounds.gameOver.pause();
+    assetLoader.sounds.gameOver2.pause();
     
     // reset score
     score = 0;
